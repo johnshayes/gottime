424122418567ea43e986e1615a4806f4e2125827
John Hayes <john@ontheinternet.org.uk>
2017-08-29 16:39:19 +0200

    meetings controller working, without particpants, basic show page no styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :listings, only: [ :index, :show, :new, :create ] do
-    resources :meetings, only: [ :new, :create, :show ]
+    resources :meetings, only: [ :create, :show ]
   end
 
 end
