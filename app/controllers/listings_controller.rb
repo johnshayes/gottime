64f8f9f44d288f3f23981b822dc17de1a38961f7
Kilian Martin <kilian.martin@me.com>
2017-08-29 12:07:16 +0200

    added basics for add listings and created controllers and routes plus fixed typo in database

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,28 @@
 class ListingsController < ApplicationController
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.create(listing_params)
+    if current_user
+      @listing.user = User.find(current_user.id)
+    else
+      @listing.user = User.find(1)
+    end
+    @listing.save
+    redirect_to listings_path
+  end
+
+  def listing_params
+    params.require(:listing).permit(:activity, :offered_datetime_text)
+  end
 end
