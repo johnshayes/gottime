321e09086c1eb1313690615316bedaf6d0c5909b
Frederike Kugland <frederike.kugland@whu.edu>
2017-08-30 15:45:01 +0200

    finally working redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
-private
-
-  def after_sign_up
-    redirect_to new_listing_path
-  end
 end
