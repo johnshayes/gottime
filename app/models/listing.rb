f7c67396484d473e4b75e37b20c4792a8b76f53f
Moritz Wahrlich <mwahrlich@users.noreply.github.com>
2017-08-31 10:50:45 +0200

    edit/delete

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,12 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_many :meetings
+
+  def owns_listing?(current_user)
+    self.user == current_user
+  end
+
+  def photo_exist?
+    self.user.facebook_picture_url == nil
+  end
 end
