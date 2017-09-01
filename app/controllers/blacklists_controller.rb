5d1db1a0d04194b54a35344b1b0024f5727af9ce
Frederike Kugland <frederike.kugland@whu.edu>
2017-09-01 12:41:56 +0200

    prepare for fuckup

--- app/controllers/blacklists_controller.rb
+++ app/controllers/blacklists_controller.rb
@@ -8,6 +8,12 @@ class BlacklistsController < ApplicationController
   end
 
   def delete
+    user_id = current_user.id
+    friend_id = User.find_by(uid: params[:friend_id]).id
+    blacklist = Blacklist.where(friend_id: friend_id).where(id: user_id)
+    blacklist.destroy
+
+    redirect_to user_path(current_user)
   end
 
 end
