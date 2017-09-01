6dbb71474a6c6fa8eb144a9fe3186a3bf32f451c
Frederike Kugland <frederike.kugland@whu.edu>
2017-09-01 16:15:55 +0200

    Black and Whitelist function ready

--- app/controllers/blacklists_controller.rb
+++ app/controllers/blacklists_controller.rb
@@ -2,16 +2,20 @@ class BlacklistsController < ApplicationController
 
   def create
     user_id = current_user.id
-    @friend_id = User.find_by(uid: params[:friend_id]).id
+    @friend_id = User.find_by(id: params[:friend_id]).id
     @blacklist = Blacklist.create(user_id: user_id, friend_id: @friend_id)
-    redirect_to user_path(current_user)
+
+    # @blacklist = Blacklist.create(user_id: user_id, friend_id: params[:friend_id])
+    # redirect_to user_path(current_user)
   end
 
-  def delete
-    user_id = current_user.id
-    friend_id = User.find_by(uid: params[:friend_id]).id
-    blacklist = Blacklist.where(friend_id: friend_id).where(id: user_id)
-    blacklist.destroy
+  def destroy
+    @blacklist = Blacklist.find(params[:id])
+    # user_id = current_user.id
+    # friend_id = User.find_by(uid: params[:friend_id]).id
+    # blacklist = Blacklist.where(friend_id: friend_id).where(id: user_id)
+    # blacklist = Blacklist.where(friend_id: params[:friend_id]).where(id: user_id)
+    @blacklist.destroy
 
     redirect_to user_path(current_user)
   end
