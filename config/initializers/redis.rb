8925002c6587ed97382a05eeb757738b3fccec48
Kilian Martin <kilian.martin@me.com>
2017-09-01 11:08:03 +0200

    feat: add config prod for redis

---
+++ config/initializers/redis.rb
@@ -0,0 +1,8 @@
+# config/initializers/redis.rb
+$redis = Redis.new
+
+url = ENV["REDISCLOUD_URL"]
+
+if url
+  $redis = Redis.new(:url => url)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
     resources :messages, only: [ :create ]
   end
 
+  mount ActionCable.server => "/cable"
 end
